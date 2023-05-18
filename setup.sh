2d20bceb77b8b569edc644f06e149d25dc791c49
nfleischmann <nils.fleischmann@outlook.com>
2023-05-18 13:44:22 +0200

    Adjust python installation

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install python and pip
-sudo yum install -y python3.10 python3.10-pip
+sudo yum install -y python python-pip
 
 # Set up Python Virtual Environment
-python3.10 -m venv env
+python -m venv env
 source env/bin/activate
 
 # Install requirements
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # Ask for environment variables
 read -p "Enter BUCKET_NAME: " bucket_name
